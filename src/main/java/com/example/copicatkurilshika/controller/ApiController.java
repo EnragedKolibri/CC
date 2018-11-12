259336eab0af81f34033f9fc4e4e174824429b82
dpolishchuk <d.polishchuk@incuube.com>
2018-11-12 16:12:28 +0200

    fix

--- src/main/java/com/example/copicatkurilshika/controller/ApiController.java
+++ src/main/java/com/example/copicatkurilshika/controller/ApiController.java
@@ -8,16 +8,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
 @Log4j2
 @RestController
+@RequestMapping("api/viber")
 public class ApiController {
 
     private Random random = new Random();
